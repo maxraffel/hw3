d2b9d3145ebdeb7f084e6a09525995c23168a068
Max Raffel <44725991+maxraffel@users.noreply.github.com>
2025-03-03 02:22:45 -0800

    heap compleap haha

--- heap.h
+++ heap.h
@@ -2,6 +2,8 @@
 #define HEAP_H
 #include <functional>
 #include <stdexcept>
+#include <vector>
+#include <iostream>
 
 template <typename T, typename PComparator = std::less<T> >
 class Heap
@@ -61,14 +63,75 @@ public:
 
 private:
   /// Add whatever helper functions and data members you need below
+  std::vector<T> data;
+  void heapify(int parent);
+  void trickle_up(int childIndex);
+  int getChildIndex(int parent, int childNumber);
+  int getParentIndex(int child);
 
-
-
+  int m;
+  PComparator c;
 
 };
 
-// Add implementation of member functions here
+template <typename T, typename PComparator>
+Heap<T, PComparator>::Heap(int m, PComparator c): m(m), c(c) {};
+template <typename T, typename PComparator>
+Heap<T, PComparator>::~Heap() {};
 
+template <typename T, typename PComparator>
+int Heap<T, PComparator>::getChildIndex(int parent, int childNumber) {
+  return m*parent + childNumber + 1;
+}
+
+template <typename T, typename PComparator>
+int Heap<T, PComparator>::getParentIndex(int childIndex) {
+  return (childIndex - 1) / m;
+}
+
+// Add implementation of member functions here
+template <typename T, typename PComparator>
+bool Heap<T, PComparator>::empty() const {
+  return data.empty();
+}
+
+template <typename T, typename PComparator>
+size_t Heap<T, PComparator>::size() const {
+  return data.size();
+}
+
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::heapify(int parent) {
+  T bestChild = data[parent];
+  int bestChildIndex = -1;
+  // find best child (which is also better than parent)
+  for (int i = 0; i < m; i++) {
+    size_t childIndex = getChildIndex(parent, i);
+    if (childIndex >= size()) continue; // stay in bounds
+    // if child is new best
+    if (c(data[childIndex], data[parent]) && c(data[childIndex], bestChild)) {
+      bestChildIndex = childIndex;
+      bestChild = data[childIndex];
+    }
+  }
+
+  // if we found somewhere to swap, swap and continue to swap down
+  if (bestChildIndex != -1) {
+    std::swap(data[parent], data[bestChildIndex]);
+    heapify(bestChildIndex);
+  }
+}
+
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::trickle_up(int childIndex) {
+  if (childIndex == 0) return;
+  int parentIndex = getParentIndex(childIndex);
+  if (c(data[childIndex], data[parentIndex])){
+    // cout << "Swapping " << data[childIndex] << "up with " << data[parentIndex] << endl;
+    std::swap(data[parentIndex], data[childIndex]);
+    trickle_up(parentIndex);
+  }
+}
 
 // We will start top() for you to handle the case of 
 // calling top on an empty heap
@@ -81,12 +144,12 @@ T const & Heap<T,PComparator>::top() const
     // ================================
     // throw the appropriate exception
     // ================================
-
+    throw std::underflow_error("Heap is empty");
 
   }
   // If we get here we know the heap has at least 1 item
   // Add code to return the top element
-
+  return data[0];
 
 
 }
@@ -101,14 +164,24 @@ void Heap<T,PComparator>::pop()
     // ================================
     // throw the appropriate exception
     // ================================
-
+    throw std::underflow_error("Heap is empty");
 
   }
-
-
+  std::swap(data[0], data[this->size() - 1]);
+  data.pop_back();
+  if (!this->empty()) {
+    heapify(0);
+  }
 
 }
 
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::push(const T& item)
+{
+  data.push_back(item);
+  trickle_up(this->size() - 1);
+
+}
 
 
 #endif
